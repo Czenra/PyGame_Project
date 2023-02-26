03d273ec4e4447d4e7ea96ff22e8d290200e3dd5
Czenra <rybkaalex2015@mail.ru>
2023-02-26 03:15:38 +0300

    base

---
+++ Main.py
@@ -0,0 +1,55 @@
+import os
+import pygame
+import sys
+
+FPS = 50
+
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ['Flappy Knight', 'Начать новую игру', 'Посмотреть лучшие результаты']
+
+    fon = pygame.transform.scale(load_image('fon.png'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font('fonts/Vinque.ttf', 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def load_game(game):
+    pass
+
+
+start_screen()
